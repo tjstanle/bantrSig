8d418f90df3d8d218d46db48b54d94ec017a683d
tjstanle <tjstanle89@gmail.com>
2025-02-05 15:00:57 -0500

    first commit

---
+++ peerServer.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
+const path = require('path'); 
+
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname+'/views');
+
+app.get('/', (req, res) => {
+    res.type('html');
+    res.render('index'); 
+  });
+  app.get('/serverTest', (req, res) => {
+    res.type('html');
+    res.render('serverTest'); 
+  });
+
+
+
+
+const server = app.listen(9000);
+
+const peerServer = ExpressPeerServer(server, {
+    key:"AknsDfy9we7rnkjsdf70ndSDGHyekjb",
+    // port:9000,
+    // debug:true,
+    // ssl: {
+        // 	// 	key: fs.readFileSync("/path/to/your/ssl/key/here.key"),
+        // 	// 	cert: fs.readFileSync("/path/to/your/ssl/certificate/here.crt"),
+        // 	// },
+	path: "/signal",
+});
+peerServer.on('connection', (client) => {console.log('New Connection',client.id);
+});
+peerServer.on('disconnect', (client) => {console.log('Connection Ended',client.id);
+});
+
+app.use("/bantr_signal", peerServer);
+
+// const fs = require("fs");
+// const { PeerServer } = require("peer");
+
+// const peerServer = PeerServer({
+// 	port: 9000,
+//     path:'/switching'
+// 	// ssl: {
+// 	// 	key: fs.readFileSync("/path/to/your/ssl/key/here.key"),
+// 	// 	cert: fs.readFileSync("/path/to/your/ssl/certificate/here.crt"),
+// 	// },
+// });

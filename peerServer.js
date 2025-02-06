c2d2a5a5441b997afa5a75b9eb9b53ee17dbc576
tjstanle <tjstanle89@gmail.com>
2025-02-06 13:33:22 -0500

    fixes

--- peerServer.js
+++ peerServer.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const { ExpressPeerServer } = require("peer");
-const path = require('path'); 
-
+const axios = require('axios').default;
+const fs = require('fs');
+// const { log } = require("console");
+// const path = require('path'); 
+// const https = require("https");
 const app = express();
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname+'/views');
 
-app.get('/', (req, res) => {
+  app.get('/', (req, res) => {
     res.type('html');
     res.render('index'); 
   });
@@ -15,25 +18,44 @@ app.get('/', (req, res) => {
     res.type('html');
     res.render('ServerTest'); 
   });
+  app.get('/health', (req, res) => {
+    res.type('json');
+    res.status(200).send({healthy:"Good"}) 
+  });
+async function postStat(status,id){
+const data = {
+  user_id:id,
+  status:status
+}
+
+try {
+    const response = await axios.post(`https://bantr.live/api/users`, data);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+
+ 
+}
 
 
 
-
-const server = app.listen(9000);
-
+const server = app.listen(443);
 const peerServer = ExpressPeerServer(server, {
     key:"AknsDfy9we7rnkjsdf70ndSDGHyekjb",
-    // port:9000,
-    // debug:true,
-    // ssl: {
-        // 	// 	key: fs.readFileSync("/path/to/your/ssl/key/here.key"),
-        // 	// 	cert: fs.readFileSync("/path/to/your/ssl/certificate/here.crt"),
-        // 	// },
+    // port:443,
+    ssl: {
+        		key: fs.readFileSync(__dirname+'/assets/bantr.pem'),
+        		cert: fs.readFileSync(__dirname+'/assets/bantr_cert.cer'),
+        	},
 	path: "/signal",
 });
-peerServer.on('connection', (client) => {console.log('New Connection',client.id);
+peerServer.on('connection', async (client) => {
+    console.log('New Connection',client.id);
+    // await postStat(true,client.id);
 });
-peerServer.on('disconnect', (client) => {console.log('Connection Ended',client.id);
+peerServer.on('disconnect', async (client) => {console.log('Connection Ended',client.id);
+    // await postStat(false,client.id);
 });
 
 app.use("/bantr_signal", peerServer);

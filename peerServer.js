9fe101591e2fac9a06ecbd9327a3bd38be5cefa5
tjstanle <tjstanle89@gmail.com>
2026-01-26 18:57:23 -0500

    fixes

--- peerServer.js
+++ peerServer.js
@@ -38,7 +38,7 @@ const peerServer = ExpressPeerServer(server, {
     key: "AknsDfy9we7rnkjsdf70ndSDGHyekjb",
     path: "/signal",
     allow_discovery: false, // PERFORMANCE: Disables peer listing, saves massive CPU/RAM
-    proxied: true,           // PERFORMANCE: Optimized for Bitnami/Apache reverse proxy
+    // proxied: true,           // PERFORMANCE: Optimized for Bitnami/Apache reverse proxy
     alive_timeout: 15000,    // PERFORMANCE: Cleans up dead connections every 15s
     concurrent_limit: 5000   // SCALABILITY: High ceiling for your t2.micro scaling
 });
